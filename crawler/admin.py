d5af17ec5c9bf0f57be361af0229080c392ce1c4
henryyang42 <henryyang42@gmail.com>
2017-03-18 21:32:11 +0800

    Admin register.

--- crawler/admin.py
+++ crawler/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Schedule)
+admin.site.register(Department)
+admin.site.register(Instructor)
+admin.site.register(Classroom)
+admin.site.register(Course)
+admin.site.register(Review)
+admin.site.register(User)
