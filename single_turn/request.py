925dd062131bc064861da0c8b6e4d749382ab99c
henryyang42 <henryyang42@gmail.com>
2017-04-02 03:15:03 +0800

    make it demo-able

--- single_turn/request.py
+++ single_turn/request.py
@@ -16,17 +16,28 @@ def query_course(goal=None, **kwargs):
         request.query_course(goal='schedule', title='機器學習技法') -> '二5,6'
     """
     ans = []
-    courses = Course.objects.filter(**kwargs).filter(semester='105-2')
-    for course in courses:
-        eval('ans.append(course.%s)' % goal)
+    if kwargs:
+        query_term = {}
+        for k, v in kwargs.items():
+            query_term[k + '__contains'] = v
 
-    return np.unique(ans)
+        courses = Course.objects.filter(**query_term).filter(semester='105-2')
+        for course in courses:
+            eval('ans.append(course.%s)' % goal)
+
+        return np.unique(ans)
+
+    return '我聽不懂您說的話耶QQ'
 
 
 def understand(input):
     tokens = [tok for tok in jieba.cut(input)]
-    print (tokens)
     intent, tokens, labels = get_intent_slot(lu_model, tokens, word2idx, idx2label, idx2intent)
 
     print (tokens, labels, intent)
+    d = {'tokens': tokens, 'labels': labels, 'intent': intent, 'slot': {}}
+    for label, token in zip(labels, tokens):
+        if label != 'O':
+            d['slot'][label[2:]] = token
 
+    return d
--- single_turn/views.py
+++ single_turn/views.py
@@ -11,16 +11,12 @@ from .request import *
 def single_turn(request):
     if request.method == 'POST':
         user_input = request.POST['input']
-        understand(user_input)
+        d = understand(user_input)
         try:
             ans = query_course(goal=d['intent'], **d['slot'])
         except:
-            ans = Course.objects.filter(semester='105-2', title__contains=user_input) | Course.objects.filter(semester='105-2', instructor__contains=user_input)
-            if ans:
-                return HttpResponse('<br>'.join([str(x) for x in ans]))
-            else:
-                return HttpResponse("Server Error: %s" % user_input)
+            pass
 
-        return HttpResponse('<br>'.join(ans))
+        return HttpResponse("%s<br><br>%s" % (str(ans), str(d)))
 
     return render(request, 'single_turn/single_turn.html', {})
