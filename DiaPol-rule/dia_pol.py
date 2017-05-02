9890dac6d27c229e8cccc9f8aabe35db0d417bd7
orina1123 <orina1123@gmail.com>
2017-05-02 23:38:38 +0800

    a very very naive dialogue policy (contraint handling not complete)

---
+++ DiaPol-rule/dia_pol.py
@@ -0,0 +1,104 @@
+#from crawler.models import *
+#from django.db.models import Q
+
+def get_action_from_frame(dia_state, courses):
+    # TODO filter courses according to dia_state
+
+    sys_act = {}
+    if len(courses) == 1: # find the unique course
+        course = courses[0]
+        sys_act["diaact"] = "inform"
+        inform_slots = {}
+        for slot in dia_state["request_slots"] + ["serial_no"]:# must provide serial_no to complete the task
+            inform_slots[slot] = course[slot]
+        sys_act["inform_slots"] = inform_slots
+        sys_act["request_slots"] = {}
+
+    elif len(courses) == 0: # fail
+        sys_act["diaact"] = "closing" 
+        sys_act["inform_slots"] = {}
+        sys_act["request_slots"] = {}
+
+    else:
+        sys_act["diaact"] = "request"
+        req_slot = None
+        max_n = 0
+        for slot in ["title", "instructor", "classroom", "schedule_str"]:
+            if slot in dia_state["request_slots"]: # don't ask users something they are asking...
+                continue
+            if slot in dia_state["inform_slots"]: # don't ask users something already known
+                continue
+            n_values = len( set( [c[slot] for c in courses] ) ) # max # different values --> largest diversity
+            if n_values > max_n:
+                max_n = n_values
+                req_slot = slot
+        sys_act["diaact"] = "request"
+        sys_act["inform_slots"] = {}
+        sys_act["request_slots"] = {req_slot:"?"}
+        
+    return sys_act
+
+
+def get_NL_from_action(sys_act):
+    res_list = []
+    for slot in ["serial_no", "title", "instructor", "classroom", "schedule_str"]: # reponse in a pre-defined order
+        if slot in sys_act["inform_slots"]: 
+            if slot == "serial_no":
+                res_str = "流水號%s。"
+            elif slot == "title":
+                res_str = "課名是%s。"
+            elif slot == "instructor":
+                res_str = "授課教師是%s。"
+            elif slot == "classroom":
+                res_str = "在%s上課。"
+            elif slot == "schedule_str":
+                res_str = "%s上課。"
+            res_str = res_str % sys_act["inform_slots"][slot]
+            res_list.append(res_str)
+
+    for slot in ["title", "instructor", "classroom", "schedule_str"]: # reponse in a pre-defined order
+        if slot in sys_act["request_slots"]: 
+            if slot == "title":
+                res_str = "請問要找哪門課?"
+            elif slot == "instructor":
+                res_str = "請問是哪位老師開的?"
+            elif slot == "classroom":
+                res_str = "請問是在哪上課的?"
+            elif slot == "schedule_str":
+                res_str = "請問是哪個時間上課的?"
+            res_list.append(res_str)
+
+    return " ".join(res_list) #TODO
+
+
+# some fake courses
+courses = [{"serial_no":"0001", "title":"機器學習技法", "instructor":"林軒田", "classroom":"資102", "schedule_str":"二5,6"}, {"serial_no":"0002", "title":"機器學習技法", "instructor":"張軒田", "classroom":"資102", "schedule_str":"二5,6"}, {"serial_no":"0003", "title":"機器學習技法", "instructor":"林軒田", "classroom":"資103", "schedule_str":"二5,6"}, {"serial_no":"0004", "title":"機器學習技法", "instructor":"林軒田", "classroom":"資102", "schedule_str":"二7,8"}]
+#courses = Course.objects.filter(semester='105-2')
+
+test_dia_states = []
+# 1. 1 request 1 inform
+dia_state = {}
+dia_state["request_slots"] = {"title":"?"}
+dia_state["inform_slots"] = {"schedule_str":"二7,8"}
+test_dia_states.append(dia_state)
+
+# 2. 1 turn to answer
+dia_state = {}
+dia_state["request_slots"] = {"schedule_str":"?"}
+dia_state["inform_slots"] = {"instructor":"張軒田"}
+test_dia_states.append(dia_state)
+
+for dia_state in test_dia_states:
+    print ("== Dialogue State ==")
+    print (dia_state)
+
+    sys_act = get_action_from_frame(dia_state, courses)
+
+    print ("== System Action ==")
+    print (sys_act)
+    NL = get_NL_from_action(sys_act)
+
+    print ("== Template-based NLG ==")
+    print (NL)
+
+    print ("----------\n")
