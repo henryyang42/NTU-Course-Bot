388fd854a297d47cff4546d76c3a8f466d2138bc
Haley <gogogo753951741963@gmail.com>
2017-05-05 03:13:32 +0800

    [Fix] Add test case

--- multiturn_LU/test_MTLU.py
+++ multiturn_LU/test_MTLU.py
@@ -8,6 +8,35 @@ from DiaPol_rule.dia_pol import *
 from user_simulator.usersim.usersim_rule import *
 from django.db.models import Q
 
+
+request_tpl = {
+    'title': [Template('請列出課程名稱'), Template('什麼課')],
+    'instructor': [Template('老師的名字'), Template('老師是誰')],
+    'schedule_str': [Template('這堂課在星期幾上課?'), Template('上課時間在什麼時候')],
+    'classroom': [Template('在哪裡上課'), Template('教室在哪')]
+}
+
+inform_tpl = {
+    'title': [Template('{{title}}'), Template('課名是{{title}}'), Template('課程名稱為{{title}}')],
+    'instructor': [Template('{{instructor}}'), Template('教學老師是{{instructor}}'), Template('教學老師是{{instructor}}'), Template('{{instructor}}上的課'), Template('是{{instructor}}教授')],
+    'schedule_str': [Template('{{when}}'), Template('上課時間是{{when}}'), Template('我想上{{when}}的課')],
+    'classroom': [Template('{{classroom}}'), Template('上課教室是{{classroom}}'), Template('在{{classroom}}上課'), Template('在{{classroom}}')]
+}
+
+
+def sem2nl(sem_in):
+    if sem_in['diaact'] == 'request':
+        attr = next(iter(sem_in['request_slots']))
+        tpl = random.choice(request_tpl[attr])
+        return tpl.render(Context(sem_in['request_slots']))
+    elif sem_in['diaact'] == 'inform':
+        attr = next(iter(sem_in['inform_slots']))
+        tpl = random.choice(inform_tpl[attr])
+        return tpl.render(Context(sem_in['inform_slots']))
+    else:
+        return '謝謝！'
+
+
 with open('user_log.pickle', 'wb') as handle:
     pickle.dump({}, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
@@ -66,6 +95,33 @@ if __name__ == '__main__':
         print('sem_fram:', sem_frame, over)
 
     user_sim = RuleSimulator(all_courses)
-    user_action = user_sim.initialize_episode()
-    print('User_action:', user_action)
-
+    action = user_sim.initialize_episode()
+    print('User_action:', action)
+    # turn 1
+    sentence = sem2nl(action)
+    status = understand(uid1, sentence)
+    action = get_action_from_frame(status)
+    print('Status:', status)
+    print('Action:', action)
+    print('sem_fram:', sem_frame, over)
+    # turn 2
+    sentence = sem2nl(user_sim.next(action)[0])
+    status = understand(uid1, sentence)
+    action = get_action_from_frame(status)
+    print('Status:', status)
+    print('Action:', action)
+    print('sem_fram:', sem_frame, over)
+    # turn 3
+    sentence = sem2nl(user_sim.next(action)[0])
+    status = understand(uid1, sentence)
+    action = get_action_from_frame(status)
+    print('Status:', status)
+    print('Action:', action)
+    print('sem_fram:', sem_frame, over)
+    # turn 4
+    sentence = sem2nl(user_sim.next(action)[0])
+    status = understand(uid1, sentence)
+    action = get_action_from_frame(status)
+    print('Status:', status)
+    print('Action:', action)
+    print('sem_fram:', sem_frame, over)
