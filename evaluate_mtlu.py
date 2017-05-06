544cc7ed378cd6ca56b943e11bfa359958c88d13
henryyang42 <henryyang42@gmail.com>
2017-05-06 19:35:12 +0800

    Scratch

---
+++ evaluate_mtlu.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+from misc_scripts import access_django
+from utils.lu import multi_turn_lu2
+from utils.nlg import *
+from user_simulator.usersim.usersim_rule import *
+from django.db.models import Q
+from django.template import Context, Template
+from crawler.models import *
+from django.conf import settings
+from LU_LSTM.lstm_predict import *
+import pickle
+
+
+all_courses = Course.objects.filter(~Q(classroom=''),~Q(instructor=''), semester='105-2').all().values()
+multi_turn_lu2('123', 'test')
+multi_turn_lu2('123', 'test')
+multi_turn_lu2('123', 'reset')
+
+for i in range(1):
+    uid = i
+    user_sim = RuleSimulator(all_courses)
+    user_action = user_sim.initialize_episode()
+
+    for _ in range(3):
+        user_sentence = agent2nl(user_action)
+        resp = {}
+        resp['sementic'], resp['status'], resp['action'], resp['resp_str'] = multi_turn_lu2(uid, user_sentence)
+        system_sentence = sem2nl(resp['action'])
+        user_action = user_sim.next(resp['action'])[0]
+        print('User  : %s' % user_sentence)
+        print('System: %s' % system_sentence)
+
