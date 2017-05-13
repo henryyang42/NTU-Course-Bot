961b4756991721da1e81c436e88dbd0ba19c700a
henryyang42 <henryyang42@gmail.com>
2017-05-13 22:21:06 +0800

    Fix toggle_rating

--- multi_turn2/urls.py
+++ multi_turn2/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from single_turn.views import toggle_rating
 from . import views
 
 urlpatterns = [
     url(r'^$', views.multi_turn, name='multi_turn'),
+    url(r'^toggle_rating/(?P<id>\d+)$', toggle_rating, name='toggle_rating')
 ]
