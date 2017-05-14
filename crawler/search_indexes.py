66b37bc0628df78c6c04cc6cfd030aff0c7f24e1
henryyang42 <henryyang42@gmail.com>
2017-05-14 20:47:17 +0800

    Remove haystack

--- crawler/search_indexes.py
+++ crawler/search_indexes.py
@@ -1,12 +1,12 @@
-from .models import Review
-from haystack import indexes
+# from .models import Review
+# from haystack import indexes
 
 
-class ReviewIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.NgramField(document=True, use_template=True)
+# class ReviewIndex(indexes.SearchIndex, indexes.Indexable):
+#     text = indexes.NgramField(document=True, use_template=True)
 
-    title = indexes.CharField(model_attr='title')
-    content = indexes.CharField(model_attr='content')
+#     title = indexes.CharField(model_attr='title')
+#     content = indexes.CharField(model_attr='content')
 
-    def get_model(self):
-        return Review
+#     def get_model(self):
+#         return Review
