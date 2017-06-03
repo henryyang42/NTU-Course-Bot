2c06feb143a2e6cc57363837f7c5f297b86bebed
Haley <gogogo753951741963@gmail.com>
2017-06-03 23:18:05 +0800

    [Fix] fix url routing

--- multi_turn2/urls.py
+++ multi_turn2/urls.py
@@ -3,7 +3,7 @@ from single_turn.views import toggle_rating
 from . import views
 
 urlpatterns = [
-    #url(r'^$', views.multi_turn, name='multi_turn'),
+    url(r'^$', views.multi_turn, name='multi_turn'),
     url(r'^rl$', views.multi_turn_rlmodel, name='multi_turn_rlmodel'),
     url(r'^toggle_rating/(?P<id>\d+)$', toggle_rating, name='toggle_rating')
 ]
--- multi_turn2/views.py
+++ multi_turn2/views.py
@@ -12,33 +12,33 @@ from utils.query import query_course
 logger = logging.getLogger(__name__)
 
 
-# def multi_turn(request):
-#     if request.method == 'POST':
-#         resp = {'resp_str': '我壞掉惹QQ'}
-#         user_input = request.POST['input']
-#         uid = request.COOKIES['csrftoken']
-#         if user_input == 'reset':
-#             user_input = '我想選課'
-#             resp['resp_str'] = '已重設'
-#             multi_turn_lu3(uid, user_input, reset=True)
-#             return HttpResponse(json.dumps(resp), content_type="application/json")
-#         try:
-#             resp['sementic'], resp['status'], resp['action'], resp['resp_str'] = multi_turn_lu3(uid, user_input)
-#             logger.debug('%s -> %s\n%s' % (user_input, resp['resp_str'], str(resp)))
-#             d_log = DialogueLog.objects.create(
-#                 utterance=user_input, reply=resp['resp_str'], debug=resp)
-#             d_group = DialogueLogGroup.objects.get(user_id=uid, group_id=resp['status']['group_id'])
-#             d_group.dialogues.add(d_log)
-#             d_group.save()
-#             resp['id'] = d_log.id
-#         except Exception:
-#             traceback.print_exc(file=sys.stdout)
-#             DialogueLog.objects.create(
-#                 utterance=user_input, debug=traceback.format_exc())
+def multi_turn(request):
+    if request.method == 'POST':
+        resp = {'resp_str': '我壞掉惹QQ'}
+        user_input = request.POST['input']
+        uid = request.COOKIES['csrftoken']
+        if user_input == 'reset':
+            user_input = '我想選課'
+            resp['resp_str'] = '已重設'
+            multi_turn_lu3(uid, user_input, reset=True)
+            return HttpResponse(json.dumps(resp), content_type="application/json")
+        try:
+            resp['sementic'], resp['status'], resp['action'], resp['resp_str'] = multi_turn_lu3(uid, user_input)
+            logger.debug('%s -> %s\n%s' % (user_input, resp['resp_str'], str(resp)))
+            d_log = DialogueLog.objects.create(
+                utterance=user_input, reply=resp['resp_str'], debug=resp)
+            d_group = DialogueLogGroup.objects.get(user_id=uid, group_id=resp['status']['group_id'])
+            d_group.dialogues.add(d_log)
+            d_group.save()
+            resp['id'] = d_log.id
+        except Exception:
+            traceback.print_exc(file=sys.stdout)
+            DialogueLog.objects.create(
+                utterance=user_input, debug=traceback.format_exc())
 
-#         return HttpResponse(json.dumps(resp), content_type="application/json")
+        return HttpResponse(json.dumps(resp), content_type="application/json")
 
-#     return render(request, 'single_turn/single_turn.html', {})
+    return render(request, 'single_turn/single_turn.html', {})
 
 
 def multi_turn_rlmodel(request):
