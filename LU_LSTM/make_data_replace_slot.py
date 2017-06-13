25258432ad27f110b64c78ea5bd179bb3a5ee9b1
orina1123 <orina1123@gmail.com>
2017-06-13 13:14:46 +0800

    small fix on log extending script

--- LU_LSTM/make_data_replace_slot.py
+++ LU_LSTM/make_data_replace_slot.py
@@ -6,14 +6,28 @@ import codecs
 import json
 import unicodecsv as csv
 import random
+import sys
+sys.path.append('..')
+from misc_scripts.access_django import *
+from utils.query import *
+from utils.tagger import *
+
+# from generate_template.py
+def trim_course(course):
+    course['when'] = random.choice(['星期', '禮拜', '周', '週']) + course['schedule_str'][0]
+    for k in ['title', 'instructor', 'classroom']:
+        course[k] = trim_attr(course[k])
+    return course
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument("dataset", type=str, help="dataset to extend from")
-ap.add_argument("db_csv", type=str, help="course database")
+#ap.add_argument("db_csv", type=str, help="course database")
 ap.add_argument("n_copy", type=int, help="make how many copies (with different slot values) for each")
 ap.add_argument("out_file", type=str, help="output interface")
 args = ap.parse_args()
 
+'''
 # load courses from CSV
 f_db = codecs.open(args.db_csv, "r")
 reader = csv.DictReader(f_db)
@@ -22,6 +36,8 @@ for row in reader:
     if row["instructor"] == "" or row["title"] == "":
         continue
     courses.append(row)
+'''
+courses = query_course({}).values()  # Get all course
 
 extend_data = [] # (intent, tokens, labels)
 with codecs.open(args.dataset, "r", "utf-8") as f_in:
@@ -31,10 +47,11 @@ with codecs.open(args.dataset, "r", "utf-8") as f_in:
         tokens = lines[i+1].strip().split(" ")
         labels = lines[i+2].strip().split(" ")
         if len(tokens) != len(labels):
-            print tokens, labels
+            print (tokens, labels)
         for _ in range(args.n_copy):
             # draw a random course
             course = random.choice(courses)
+            course = trim_course(course)
             new_tokens = []
             new_labels = []
             for j in range(0, len(tokens)):
@@ -52,7 +69,7 @@ with codecs.open(args.dataset, "r", "utf-8") as f_in:
                         if " " in t:
                             t = t.replace(" ", "")
                     elif slot == "when":
-                        t = random.choice(['星期', '禮拜', '週']) + course['schedule_str'][0]
+                        t = random.choice(['星期', '禮拜', '週', '周']) + course['schedule_str'][0]
 
 
                 new_tokens.append(t)
